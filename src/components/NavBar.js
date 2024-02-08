46b799e4c0b1cc4d94c2fa14d6de742e042cd00b
karaneliab <eliabkaran7884@gmail.com>
2024-02-08 13:03:30 +0300

    deliverables met

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,30 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  const navLinks = ['/movies', '/directors','/actors']
+  return (
+  <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="navbar-nav mr-auto">
+              <li>
+              <NavLink to="/">Home</NavLink>
+             
+              </li>
+              <li>
+              <NavLink to="/actors">Actors</NavLink>
+              </li>
+              <li>
+              <NavLink to="/directors">Directors</NavLink>
+              </li>
+              <li>
+              <NavLink to="/movies">Movies</NavLink>
+              </li>
+            </ul>
+
+        </nav>
+  </div>
+
+  )
 }
 
 export default NavBar;
